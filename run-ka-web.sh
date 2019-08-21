3c71876062c77dd6123d26fe8119152a33d0ad9b
communityus <37026425+communityus@users.noreply.github.com>
2019-08-21 16:48:24 -0600

    diff port and assets folder

--- run-ka-web.sh
+++ run-ka-web.sh
@@ -1,5 +1,5 @@
 docker run -it --rm \
-    -p ${KA_CLIENT_LISTEN:-0.0.0.0}:${KA_CLIENT_PORT:-8080}:80  \
+    -p ${KA_CLIENT_LISTEN:-0.0.0.0}:${KA_CLIENT_PORT:-8090}:80  \
     -v ${PWD}/app:/src/app                              \
     -v ${PWD}/gulp-tasks:/src/gulp-tasks                \
     -v ${PWD}/index.html:/src/index.html                \
