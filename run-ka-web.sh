1663b02502cbf21a722e0be9d6d72c4018c8d2aa
communityus <37026425+communityus@users.noreply.github.com>
2019-08-21 16:47:58 -0600

    Update run-ka-web.sh

--- run-ka-web.sh
+++ run-ka-web.sh
@@ -3,7 +3,7 @@ docker run -it --rm \
     -v ${PWD}/app:/src/app                              \
     -v ${PWD}/gulp-tasks:/src/gulp-tasks                \
     -v ${PWD}/index.html:/src/index.html                \
-    -v ${PWD}/lacuna/assets:/src/lacuna/assets \
+    -v ${PWD}/lacuna/assets:/src/assets \
     -v ${PWD}/app/css/styles.css:/src/lacuna/styles.css \
     -v ${PWD}/app/js/load.js:/src/lacuna/load.js        \
     -v ${PWD}/app/vendor-css:/src/app/vendor-css        \
